42f3ba0c2d06077144bb67e4bf8fd5d2f4592f02
Davy Jones <nikitayashin3@gmail.com>
2023-08-24 01:10:19 +0300

    base styles & theme context

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import { Suspense } from "react";
-import { Route, Routes, Link } from "react-router-dom";
+import { Suspense, useContext } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import "./index.scss";
+import { useTheme } from "./theme/useTheme";
+
+import "./styles/index.scss";
 
 const App = () => {
-  return (
-    <div className="app">
-      <Link to="/about">О сайте</Link>
-      <Link to="/main">Главная</Link>
+  const { theme, toggleTheme } = useTheme()
 
+  return (
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path="/about" element={<AboutPageAsync />} />
-          <Route path="/main" element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
         </Routes>
       </Suspense>
     </div>

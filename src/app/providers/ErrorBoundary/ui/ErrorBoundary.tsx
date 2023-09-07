ac8bf4d04afb7fe28a9607531f9b50a8caea7371
Davy Jones <nikitayashin3@gmail.com>
2023-09-08 01:38:27 +0300

    add error boundary & page rror

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,44 @@
+import React, { ErrorInfo, ReactNode, Suspense } from "react";
+import { PageError } from "widgets/PageError";
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+  // eslint-disable-next-line n/handle-callback-err
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: ErrorInfo) {
+    // You can also log the error to an error reporting service
+    console.log(error, info);
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+
+    if (hasError) {
+      return (
+        <Suspense fallback="">
+          <PageError />
+        </Suspense>
+      );
+    }
+
+    return children;
+  }
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -3,7 +3,7 @@ import path from "path";
 import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
 import { BuildEnv, BuildPaths } from "./config/build/types/config";
 
-export default (env: BuildEnv) => {
+export default (env: BuildEnv): webpack.Configuration => {
   const paths: BuildPaths = {
     entry: path.resolve(__dirname, "src", "index.tsx"),
     build: path.resolve(__dirname, "build"),
